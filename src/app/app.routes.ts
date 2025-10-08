8bf15f7144f1229b15efbcd7f85d01b85c9ff09e
mehdi-larek <larek.med@gmail.com>
2025-10-08 12:45:18 +0200

    wip signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Signup } from './auth/signup/signup';
 
 export const routes: Routes = [
     // { path: 'films', component: FilmsComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+    { path: 'signup', component: Signup },
+    
 ];
---
+++ src/app/auth/signup/signup.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Title } from "../../components/title/title";
+
+@Component({
+  selector: 'app-signup',
+  imports: [
+    Title
+  ],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css'
+})
+export class Signup {
+
+}
