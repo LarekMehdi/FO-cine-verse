b5a5d5d52e2bd1766a6cf32b8413cd64c74ddc04
mehdi-larek <larek.med@gmail.com>
2025-11-11 13:29:34 +0100

    plusiurs couleurs suivant les toast

--- src/app/components/shared/toast/toast.ts
+++ src/app/components/shared/toast/toast.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ToastService } from '../../../service/toast.service';
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-toast',
-  imports: [],
+  imports: [NgClass],
   standalone: true,
   templateUrl: './toast.html',
   styleUrl: './toast.css'
--- src/app/service/toast.service.ts
+++ src/app/service/toast.service.ts
@@ -4,27 +4,30 @@ import { Injectable, signal } from "@angular/core";
 export class ToastService {
 
     public message = signal<string | null>(null);
+    public type = signal<'success' | 'error' | 'info' | null>(null);
 
-    private show(msg: string) {
+    private show(msg: string, type: 'success' | 'error' | 'info') {
         this.message.set(msg);
+        this.type.set(type);
         setTimeout(() => {
             this.message.set(null);
+            this.type.set(null);
         }, 3000); // 3 secondes
     }
 
     public success(message: string) {
-        this.show(message);
+        this.show(message, 'success');
     }
 
     public error(message: string) {
-        this.show(message);
+        this.show(message, 'error');
     }
 
     public defaultError() {
-        this.show('Une erreur est survenue');
+        this.show('Une erreur est survenue', 'error');
     }
 
     public info(message: string) {
-        this.show(message);
+        this.show(message, 'info');
     }
 }
